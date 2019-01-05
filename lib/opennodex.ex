f63516631dd1eba0ca82227f0ea093a9691a13b1
Bradford Folkens <bfolkens@gmail.com>
2019-01-05 17:55:10 -0600

    Initial commit

---
+++ lib/opennodex.ex
@@ -0,0 +1,18 @@
+defmodule OpenNodex do
+  @moduledoc """
+  Documentation for OpenNodex.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> OpenNodex.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/opennodex_test.exs
@@ -0,0 +1,8 @@
+defmodule OpenNodexTest do
+  use ExUnit.Case
+  doctest OpenNodex
+
+  test "greets the world" do
+    assert OpenNodex.hello() == :world
+  end
+end
