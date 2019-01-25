06ecdd14916265de6dbdc5e4005493f5534be1f4
Bradford Folkens <bfolkens@gmail.com>
2019-01-25 09:00:52 -0800

    Fix docs

--- lib/opennodex/event.ex
+++ lib/opennodex/event.ex
@@ -22,33 +22,33 @@ defmodule OpenNodex.Event do
   def from(%{} = m), do: struct(__MODULE__, m)
 
   @doc """
-  Checks the validity of the order hash for the `charge` and `api_key`.
+  Checks the validity of the order hash for the `event` and `api_key`.
 
     ## Examples
 
-        iex> charge = %OpenNodex.Charge{
+        iex> event = %OpenNodex.Event{
         ...>   id: "abbacadabba-d123-456a-baba-99bfdcfb16a1",
         ...>   hashed_order: "df873d17dd8b6335db232ed1c242d4df91b0dbe3b3f2baba67e7d38c43b8370c"
         ...> }
-        iex> OpenNodex.Charge.check_validity(charge, "abbacadabba")
+        iex> OpenNodex.Event.check_validity(event, "abbacadabba")
         true
 
   """
-  def check_validity(charge, api_key) do
-    charge.hashed_order == order_hash(charge, api_key)
+  def check_validity(event, api_key) do
+    event.hashed_order == order_hash(event, api_key)
   end
 
   @doc """
-  Computes the order hash (in base 16) for the `charge` and `api_key`.
+  Computes the order hash (in base 16) for the `event` and `api_key`.
 
   ## Examples
 
-      iex> charge = %OpenNodex.Charge{id: "abbacadabba-d123-456a-baba-99bfdcfb16a1"}
-      iex> OpenNodex.Charge.order_hash(charge, "abbacadabba")
+      iex> event = %OpenNodex.Event{id: "abbacadabba-d123-456a-baba-99bfdcfb16a1"}
+      iex> OpenNodex.Event.order_hash(event, "abbacadabba")
       "df873d17dd8b6335db232ed1c242d4df91b0dbe3b3f2baba67e7d38c43b8370c"
 
   """
-  def order_hash(charge, api_key) do
-    :crypto.hmac(:sha256, api_key, charge.id) |> Base.encode16(case: :lower)
+  def order_hash(event, api_key) do
+    :crypto.hmac(:sha256, api_key, event.id) |> Base.encode16(case: :lower)
   end
 end
